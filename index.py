2b5d5c680fb2e5dcc8e651e7e43865981823076c
zyfy29 <wasuremono127@gmail.com>
2025-08-16 19:54:09 +0900

    feat: add CI workflow for testing and update dependencies

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 
 import requests
--- tests/test_ui.py
+++ tests/test_ui.py
@@ -7,11 +7,11 @@ from pytest_mock import MockerFixture
 
 @pytest.fixture
 def app(mocker: MockerFixture):
-    at = AppTest.from_file("../index.py")
+    at = AppTest.from_file("index.py")
     at.secrets["data_url"] = "https://www.example.com/issues.json"
     mock_get = mocker.patch("requests.get")
     mock_get.return_value.status_code = 200
-    with open("data.json", "r") as f:
+    with open("tests/data.json", "r") as f:
         mock_get.return_value.json.return_value = json.load(f)
     return at
 
@@ -21,14 +21,35 @@ def test_run(app):
     assert not app.exception
 
 
-def test_countup_caption(app):
+def test_countup(app):
     app.run()
     assert len(app.caption) > 0
     assert app.caption[0].value == "Found 10 issues."
 
 
-def test_hide_assigned_toggle(app):
+def test_hide_assigned(app):
     app.run()
-    # fixme
-    # app.sidebar.toggle[0].set_value(True).run()
-    # assert app.caption[0].value == "Found 10 issues."
+    app.sidebar.toggle[1].set_value(True).run()  # Hide Assigned
+    assert app.caption[0].value == "Found 5 issues."
+
+
+def test_language_multiselect_one(app):
+    app.run()
+    app.sidebar.multiselect[0].set_value(["Go"]).run()
+    assert len(app.caption) > 0
+    assert app.caption[0].value == "Found 6 issues."
+
+
+def test_language_multiselect_multiple_and(app):
+    app.run()
+    app.sidebar.multiselect[0].set_value(["Go", "Python"]).run()
+    assert len(app.caption) > 0
+    assert app.caption[0].value == "Found 1 issues."
+
+
+def test_language_multiselect_multiple_or(app):
+    app.run()
+    app.sidebar.multiselect[0].set_value(["Go", "Python"]).run()
+    app.sidebar.toggle[0].set_value(True).run()  # Allow Either
+    assert len(app.caption) > 0
+    assert app.caption[0].value == "Found 10 issues."
